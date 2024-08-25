141dedd7fcafcade74b0687d460bb64b718e6a3b
EFFemole <go.netgle@gmail.com>
2024-08-25 10:35:51 +0300

    update forecast days

--- feature/openweatherapi/src/main/java/com/example/openweatherapi/api/OpenWeatherApiService.kt
+++ feature/openweatherapi/src/main/java/com/example/openweatherapi/api/OpenWeatherApiService.kt
@@ -4,13 +4,16 @@ import com.example.openweatherapi.model.WeatherData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+private const val DefaultForecastDays = 7
+
 /**
  *  Interface representing the API service for fetching weather forecast data.
  */
 interface OpenWeatherApiService {
-    @GET("forecast?hourly=temperature_2m,relative_humidity_2m,weather_code&timezone=auto&forecast_days=16")
+    @GET("forecast?hourly=temperature_2m,relative_humidity_2m,weather_code&timezone=auto")
     suspend fun getForecast(
         @Query("latitude") lat: Double,
         @Query("longitude") lon: Double,
+        @Query("forecast_days") days: Int = DefaultForecastDays
     ): WeatherData
 }
