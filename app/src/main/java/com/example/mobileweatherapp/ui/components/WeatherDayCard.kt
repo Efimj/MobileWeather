db4a90e021f4cfce0d551b6adab21c3a59647988
EFFemole <go.netgle@gmail.com>
2024-09-07 22:13:05 +0300

    update ui

--- app/src/main/java/com/example/mobileweatherapp/ui/components/WeatherDayCard.kt
+++ app/src/main/java/com/example/mobileweatherapp/ui/components/WeatherDayCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -87,7 +88,7 @@ fun WeatherDayCard(
                 painter = painterResource(weatherImage),
                 contentDescription = null
             )
-            HorizontalSpacer(value = 20.dp)
+            Spacer(modifier = Modifier.width(30.dp))
             Text(
                 text = temperatureText,
                 style = MaterialTheme.typography.titleMedium,
